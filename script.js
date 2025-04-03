792eb1a43ebbdd04674c2fabac0b29a6a218cdb8
kaiofmuniz <160793475+kaiofmuniz@users.noreply.github.com>
2025-04-03 19:28:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-document.addEventListener('DOMContentLoaded' , function(){
+document.addEventListener('DOMContentLoaded',function(){
 
-const botaoDeAcessibilidade=document.getElementById('bot-acessibilidade');
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessbilidade=document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+    
+    const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
+    
+    botaoDeAcessibilidade.setAttribute('aria-expended' , botaoSelecionado)
 
-})
+    })
+    
+    
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte')
+    const diminiFonteBotao = document.getElementById('diminir-fonte')
+    
+    const alternaconstraste = document.getElementById('alterna-constraste')
+    
+    
+    })
